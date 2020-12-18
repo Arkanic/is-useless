353a8bfb965b07ac87b63e58553c5eba964bde58
Arkanic <50847107+Arkanic@users.noreply.github.com>
2020-12-19 07:13:07 +1300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,5 @@ module.exports = function (v) {
   for (var i = 0; i < 100; i++) {
     v = Math.floor(Math.sqrt(Math.random())); // don't touch. It works.
   }
-  while (isTrue(v === true) === false) v = Math.random() < 1 ? true : false;
   return v;
 };
