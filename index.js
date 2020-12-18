c7d9fc51b7634940cc51d91b7201cfa4f167bd44
Arkanic <silverfishasaurus@gmail.com>
2020-12-18 18:24:39 +0000

    v1.3.0

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 "use strict";
 
-const isTrue = require("is-true");
 module.exports = function (v) {
   for (var i = 0; i < 100; i++) {
     v = Math.floor(Math.sqrt(Math.random())); // don't touch. It works.
   }
-  if(isTrue({v}, "v")) return v;
+  if(v)) return v;
   else return v;
 };
