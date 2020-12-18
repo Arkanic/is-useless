179f52e1fd91dc71b06c331c0a5c934b9b1016af
Arkanic <50847107+Arkanic@users.noreply.github.com>
2020-12-18 18:22:41 +1300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 const isTrue = require("is-true");
 module.exports = function (v) {
-  if (v) throw new TypeError("Expected any kind of information, got null");
   for (var i = 0; i < 100; i++) {
     v = Math.floor(Math.sqrt(Math.random())); // slow down code, it runs too fast
   }
