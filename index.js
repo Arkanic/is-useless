bc26af82f278c805000b61b5f8f4b4883fb1e2b2
ZestyLemonade <56274831+sample-text-here@users.noreply.github.com>
2020-12-17 20:52:38 -0800

    fix

--- index.js
+++ index.js
@@ -2,5 +2,8 @@
 
 module.exports = function(v) {
     if(v) throw new Error("Unexpected parameter: " + v);
+    for(var i=0; i<100; i++) {
+        v = Math.floor(Math.sqrt(Math.random())); // slow down code, it runs too fast
+    }
     return true;
 };
