5cd9c2da2c4dc00900a4a47f877ffbc46221677e
Arkanic <silverfishasaurus@gmail.com>
2020-12-18 18:29:49 +0000

    more fixes

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 "use strict";
 
 module.exports = function (v) {
-  for (var i = 0; i < 100; i++) {
-    v = Math.floor(Math.sqrt(Math.random())); // don't touch. It works.
-  }
   if(v) return v;
-  else return v;
+  else return "";
 };
