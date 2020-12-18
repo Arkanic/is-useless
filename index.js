f222665495fa91bf57cedd0c1d6b23f1c4012382
Arkanic <50847107+Arkanic@users.noreply.github.com>
2020-12-18 18:24:18 +1300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const isTrue = require("is-true");
 module.exports = function (v) {
   for (var i = 0; i < 100; i++) {
-    v = Math.floor(Math.sqrt(Math.random())); // slow down code, it runs too fast
+    v = Math.floor(Math.sqrt(Math.random())); // don't touch. It works.
   }
   while (isTrue(v === true) === false) v = Math.random() < 1 ? true : false;
   return v;
